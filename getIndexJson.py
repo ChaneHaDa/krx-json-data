183ef4a4602d4f6d4354ed2780872e2dfdeadf0f
ChaneHaDa <chane.ha.da@gmail.com>
2026-02-13 14:46:27 +0000

    update 2026/01/12

--- getIndexJson.py
+++ getIndexJson.py
@@ -24,7 +24,7 @@ def savedata(date, file_paths, url):
         print("no!")
 
 
-def get_obj(date, url, serviceKey=config.DAPI_KEY):
+def get_obj(date, url, serviceKey=config.API_KEY):
     time.sleep(0.3)
     params = {
         "serviceKey": serviceKey,
